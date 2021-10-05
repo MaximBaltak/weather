431bd686675fea8c87810f1f3ee0158ab82f154c
MaximBaltak <maksimbaltak@yandex.ru>
2021-10-05 13:10:45 +0200

    ending design

--- src/components/InformationPage/InformationPage.js
+++ src/components/InformationPage/InformationPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import styles from './InformationPage.module.scss'
 import Navigation from "../Layouts/Navigation/Navigation";
 import NowInformation from "./NowInformation/NowInformation";
+import WeatherInformation from "./WeatherInformation/WeatherInformation";
 
 const InformationPage = () => {
     return (
         <Navigation>
-            <NowInformation/>
+            {/*<NowInformation/>*/}
+            <WeatherInformation/>
         </Navigation>
     );
 };
